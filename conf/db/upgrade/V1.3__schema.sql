936d24d2029fa033d301a306820dddddacb7c106
Frank Zhang <xing5820@gmail.com>
2016-04-24 00:00:19 +0800

    working on actual size of image

--- conf/db/upgrade/V1.3__schema.sql
+++ conf/db/upgrade/V1.3__schema.sql
@@ -1,3 +1,4 @@
 ALTER TABLE ImageEO ADD actualSize bigint unsigned DEFAULT NULL;
 DROP VIEW IF EXISTS `zstack`.`ImageVO`;
 CREATE VIEW `zstack`.`ImageVO` AS SELECT uuid, name, description, status, state, size, actualSize, md5Sum, platform, type, format, url, system, mediaType, createDate, lastOpDate, guestOsType FROM `zstack`.`ImageEO` WHERE deleted IS NULL;
+UPDATE ImageVO set actualSize = size;
