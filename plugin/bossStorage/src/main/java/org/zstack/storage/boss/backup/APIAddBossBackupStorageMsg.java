4a743e80dba4d95894761fb6b126d17d82828e9b
xuxuping <nitetexp@gmail.com>
2016-11-07 10:26:15 +0800

    add db VO in persistance.xml and complete APIAddBossBackupMsg

--- plugin/bossStorage/src/main/java/org/zstack/storage/boss/backup/APIAddBossBackupStorageMsg.java
+++ plugin/bossStorage/src/main/java/org/zstack/storage/boss/backup/APIAddBossBackupStorageMsg.java
@@ -1,9 +1,35 @@
 package org.zstack.storage.boss.backup;
 
+import org.zstack.header.message.APIParam;
+import org.zstack.header.message.OverriddenApiParam;
+import org.zstack.header.message.OverriddenApiParams;
 import org.zstack.header.storage.backup.APIAddBackupStorageMsg;
+import org.zstack.storage.boss.BossConstants;
 
 /**
  * Created by XXPS-PC1 on 2016/10/28.
  */
+@OverriddenApiParams({
+        @OverriddenApiParam(field = "url", param = @APIParam(maxLength = 2048, required = false))
+})
 public class APIAddBossBackupStorageMsg extends APIAddBackupStorageMsg {
+    @APIParam(required = false, maxLength = 255)
+    private String poolName;
+
+    public String getUrl() {
+        return "not used";
+    }
+
+    public String getPoolName() {
+        return poolName;
+    }
+
+    public void setPoolName(String poolName) {
+        this.poolName = poolName;
+    }
+
+    @Override
+    public String getType() {
+        return BossConstants.BOSS_BACKUP_STORAGE_TYPE;
+    }
 }
