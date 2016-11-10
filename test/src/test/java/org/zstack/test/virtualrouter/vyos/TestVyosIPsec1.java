a801a18ea84cfc9d138ed6c6ca73bb5887689812
Frank Zhang <xing5820@gmail.com>
2016-11-10 14:10:41 +0800

    VPN support #1068
    for https://github.com/zxwing/premium/issues/1068

--- test/src/test/java/org/zstack/test/virtualrouter/vyos/TestVyosIPsec1.java
+++ test/src/test/java/org/zstack/test/virtualrouter/vyos/TestVyosIPsec1.java
@@ -34,7 +34,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 /**
- * test create ipsec connection
+ *  test create ipsec connection
  */
 public class TestVyosIPsec1 {
     Deployer deployer;
