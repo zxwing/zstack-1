4ba1821148d07a058812b1795c3f3e0ff01ef910
Frank Zhang <xing5820@gmail.com>
2016-11-10 14:10:41 +0800

    VPN support #1068
    for https://github.com/zxwing/premium/issues/1068

--- test/src/test/java/org/zstack/test/virtualrouter/vyos/TestVyosIPsec1.java
+++ test/src/test/java/org/zstack/test/virtualrouter/vyos/TestVyosIPsec1.java
@@ -21,7 +21,6 @@ import org.zstack.ipsec.vyos.VyosIPsecBackend.IPsecInfo;
 import org.zstack.ipsec.vyos.VyosIPsecSimulatorConfig;
 import org.zstack.network.service.vip.VipInventory;
 import org.zstack.network.service.vip.VipVO;
-import org.zstack.network.service.virtualrouter.VirtualRouterVmVO;
 import org.zstack.network.service.virtualrouter.vyos.VyosConstants;
 import org.zstack.simulator.appliancevm.ApplianceVmSimulatorConfig;
 import org.zstack.simulator.virtualrouter.VirtualRouterSimulatorConfig;
@@ -185,9 +184,7 @@ public class TestVyosIPsec1 {
         Assert.assertEquals(1, iconfig.createIPsecConnectionCmdList.size());
         CreateIPsecConnectionCmd cmd = iconfig.createIPsecConnectionCmdList.get(0);
         Assert.assertEquals(1, cmd.infos.size());
-        IPsecInfo info = cmd.infos.get(0);
-        compare(ipsec, null, info);
-        Assert.assertTrue(info.excludeSnat);
+        compare(ipsec, null, cmd.infos.get(0));
 
         VipVO vipvo = dbf.findByUuid(vip.getUuid(), VipVO.class);
         Assert.assertEquals(IPsecConstants.IPSEC_NETWORK_SERVICE_TYPE.toString(), vipvo.getUseFor());
@@ -203,13 +200,5 @@ public class TestVyosIPsec1 {
         Assert.assertEquals(0, count);
         count = dbf.count(IPsecPeerCidrVO.class);
         Assert.assertEquals(0, count);
-
-        // test host reconnect is not effected by the flat network provider
-        HostInventory host1 = deployer.hosts.get("host1");
-        api.reconnectHost(host1.getUuid());
-
-        // test the vr is set to never stop
-        VirtualRouterVmVO vr = dbf.listAll(VirtualRouterVmVO.class).get(0);
-        Assert.assertTrue(HaSystemTags.HA.hasTag(vr.getUuid()));
     }
 }
