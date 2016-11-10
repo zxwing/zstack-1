46c89e7941fdd3bbfaddf71237fec60a087dafa3
xuxuping <nitetexp@gmail.com>
2016-11-10 15:35:11 +0800

    modify the BossBackupStorageVO

--- plugin/bossStorage/src/main/java/org/zstack/storage/boss/backup/BossBackupStorageVO.java
+++ plugin/bossStorage/src/main/java/org/zstack/storage/boss/backup/BossBackupStorageVO.java
@@ -50,5 +50,6 @@ public class BossBackupStorageVO extends BackupStorageVO{
 
     public BossBackupStorageVO(BossBackupStorageVO other) {
         super(other);
+        this.clusterName = other.getClusterName();
     }
 }
