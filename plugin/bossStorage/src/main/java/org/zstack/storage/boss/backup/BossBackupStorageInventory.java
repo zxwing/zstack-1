2be7297d9edc76cc8dbe62fbfad3aa9fb886768c
xuxuping <nitetexp@gmail.com>
2016-11-29 10:17:15 +0800

    modify the BossBackupStorageInventory

--- plugin/bossStorage/src/main/java/org/zstack/storage/boss/backup/BossBackupStorageInventory.java
+++ plugin/bossStorage/src/main/java/org/zstack/storage/boss/backup/BossBackupStorageInventory.java
@@ -1,6 +1,9 @@
 package org.zstack.storage.boss.backup;
 
+import org.zstack.header.search.Inventory;
+import org.zstack.header.search.Parent;
 import org.zstack.header.storage.backup.BackupStorageInventory;
+import org.zstack.storage.boss.BossConstants;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -9,6 +12,9 @@ import java.util.List;
 /**
  * Created by XXPS-PC1 on 2016/11/9.
  */
+@Inventory(mappingVOClass = BossBackupStorageVO.class, collectionValueOfMethod = "valueOf1",
+        parent = {@Parent(inventoryClass = BackupStorageInventory.class, type = BossConstants.BOSS_BACKUP_STORAGE_TYPE)}
+)
 public class BossBackupStorageInventory extends BackupStorageInventory{
 
     private String clusterName;
