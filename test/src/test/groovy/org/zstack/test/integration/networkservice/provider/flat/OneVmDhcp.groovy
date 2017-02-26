38e455c12c2e7a6706aeb964bf4e6325e70206f4
Frank.Zhang <xin.zhang@mevoco.com>
2017-02-26 02:56:16 -0500

    complete flat dhcp test case

--- test/src/test/groovy/org/zstack/test/integration/networkservice/provider/flat/OneVmDhcp.groovy
+++ test/src/test/groovy/org/zstack/test/integration/networkservice/provider/flat/OneVmDhcp.groovy
@@ -4,6 +4,7 @@ import org.springframework.http.HttpEntity
 import org.zstack.header.network.l3.UsedIpVO
 import org.zstack.network.service.flat.BridgeNameFinder
 import org.zstack.network.service.flat.FlatDhcpBackend
+import org.zstack.network.service.flat.FlatDhcpBackend.DhcpInfo
 import org.zstack.network.service.flat.FlatNetworkSystemTags
 import org.zstack.sdk.L3NetworkInventory
 import org.zstack.sdk.VmInstanceInventory
@@ -15,15 +16,10 @@ import org.zstack.testlib.L3NetworkSpec
 import org.zstack.testlib.SubCase
 import org.zstack.utils.gson.JSONObjectUtil
 
-/**
- * Created by xing5 on 2017/2/26.
- */
 class OneVmDhcp extends SubCase {
     EnvSpec env
 
     VmInstanceInventory vm
-    String dhcpServerIp
-    String dhcpServerIpUuid
     L3NetworkInventory l3
 
     @Override
@@ -42,10 +38,115 @@ class OneVmDhcp extends SubCase {
             l3 = (env.specByName("l3") as L3NetworkSpec).inventory
 
             testSetDhcpWhenCreateVm()
+            testReleaseDhcpWhenStopVm()
+            testSetDhcpWhenStartVm()
+            testSetDhcpReleaseDhcpWhenRebootVm()
+            testSetDhcpWhenReconnectHost()
+            testReleaseDhcpWhenDestroyVm()
+            testDeleteNamespaceAndDhcpIpWhenDeleteL3Network()
         }
     }
 
-    void testSetDhcpWhenCreateVm() {
+    void testDeleteNamespaceAndDhcpIpWhenDeleteL3Network() {
+        FlatDhcpBackend.DeleteNamespaceCmd cmd = null
+
+        def dhcpServerIpUuid = FlatNetworkSystemTags.L3_NETWORK_DHCP_IP
+                .getTokenByResourceUuid(l3.uuid, FlatNetworkSystemTags.L3_NETWORK_DHCP_IP_UUID_TOKEN)
+        String brName = new BridgeNameFinder().findByL3Uuid(l3.uuid)
+
+        env.afterSimulator(FlatDhcpBackend.DHCP_DELETE_NAMESPACE_PATH) { rsp, HttpEntity<String> e ->
+            cmd = JSONObjectUtil.toObject(e.body, FlatDhcpBackend.DeleteNamespaceCmd.class)
+            return rsp
+        }
+
+        deleteL3Network {
+            uuid = l3.uuid
+        }
+
+        assert cmd != null
+        assert cmd.namespaceName != null
+
+        assert cmd.bridgeName == brName
+        // make sure the DHCP server IP has been returned
+        assert dbFindByUuid(dhcpServerIpUuid, UsedIpVO.class) == null
+
+        // assure relevant tags are deleted
+        assert !FlatNetworkSystemTags.L3_NETWORK_DHCP_IP.hasTag(l3.uuid)
+        assert new BridgeNameFinder().findByL3Uuid(l3.uuid, false) == null
+    }
+
+    void testSetDhcpWhenReconnectHost() {
+        testSetDhcpWhenVmOperations {
+            reconnectHost {
+                uuid = vm.hostUuid
+            }
+        }
+    }
+
+    void testReleaseDhcpWhenDestroyVm() {
+        testReleseDhcpWhenVmOperations {
+            destroyVmInstance {
+                uuid = vm.uuid
+            }
+        }
+    }
+
+    void testSetDhcpReleaseDhcpWhenRebootVm() {
+        testReleseDhcpWhenVmOperations {
+            rebootVmInstance {
+                uuid = vm.uuid
+            }
+        }
+
+        testSetDhcpWhenVmOperations {
+            rebootVmInstance {
+                uuid = vm.uuid
+            }
+        }
+    }
+
+    void testSetDhcpWhenStartVm() {
+        testSetDhcpWhenVmOperations {
+            startVmInstance {
+                uuid = vm.uuid
+            }
+        }
+    }
+
+    void assertDhcpInfoVmNic(DhcpInfo info, VmNicInventory nic) {
+        assert nic.ip == info.ip
+        assert nic.netmask == info.netmask
+        assert nic.gateway == info.gateway
+        assert info.isDefaultL3Network
+        assert info.l3NetworkUuid == nic.l3NetworkUuid
+    }
+
+    private testReleseDhcpWhenVmOperations(Closure vmOperation) {
+        FlatDhcpBackend.ReleaseDhcpCmd cmd = null
+
+        env.afterSimulator(FlatDhcpBackend.RELEASE_DHCP_PATH) { rsp, HttpEntity<String> e ->
+            cmd = JSONObjectUtil.toObject(e.body, FlatDhcpBackend.ReleaseDhcpCmd.class)
+            return rsp
+        }
+
+        vmOperation()
+
+        assert cmd != null
+        assert cmd.dhcp.size() == 1
+        DhcpInfo info = cmd.dhcp[0]
+        VmNicInventory nic = vm.vmNics[0]
+        assertDhcpInfoVmNic(info, nic)
+    }
+
+    void testReleaseDhcpWhenStopVm() {
+        testReleseDhcpWhenVmOperations {
+            stopVmInstance {
+                uuid = vm.uuid
+            }
+        }
+    }
+
+    private void testSetDhcpWhenVmOperations(Closure vmOperation) {
         FlatDhcpBackend.ApplyDhcpCmd cmd = null
 
         env.afterSimulator(FlatDhcpBackend.APPLY_DHCP_PATH) { rsp, HttpEntity<String> e ->
@@ -59,27 +160,16 @@ class OneVmDhcp extends SubCase {
             return rsp
         }
 
-        ImageSpec image = env.specByName("image")
-        InstanceOfferingSpec instanceOffering = env.specByName("instanceOffering")
-
-        vm = createVmInstance {
-            name = "vm"
-            imageUuid = image.inventory.uuid
-            l3NetworkUuids = [l3.uuid]
-            instanceOfferingUuid = instanceOffering.inventory.uuid
-        }
+        vmOperation()
 
         // check ApplyDhcpCmd
         assert cmd != null
         assert cmd.l3NetworkUuid == l3.uuid
         assert cmd.dhcp.size() == 1
 
-        FlatDhcpBackend.DhcpInfo info = cmd.dhcp[0]
+        DhcpInfo info = cmd.dhcp[0]
         VmNicInventory vmNic = vm.vmNics[0]
-        assert vmNic.ip == info.ip
-        assert vmNic.netmask == info.netmask
-        assert vmNic.gateway == info.gateway
-        assert info.isDefaultL3Network
+        assertDhcpInfoVmNic(info, vmNic)
 
         String brName = new BridgeNameFinder().findByL3Uuid(l3.uuid)
         assert brName == info.bridgeName
@@ -94,8 +184,8 @@ class OneVmDhcp extends SubCase {
         // check the DHCP IP
         // the DHCP server will occupy an IP
         def tokens = FlatNetworkSystemTags.L3_NETWORK_DHCP_IP.getTokensByResourceUuid(l3.uuid)
-        dhcpServerIp = tokens.get(FlatNetworkSystemTags.L3_NETWORK_DHCP_IP_TOKEN)
-        dhcpServerIpUuid = tokens.get(FlatNetworkSystemTags.L3_NETWORK_DHCP_IP_UUID_TOKEN)
+        String dhcpServerIp = tokens.get(FlatNetworkSystemTags.L3_NETWORK_DHCP_IP_TOKEN)
+        String dhcpServerIpUuid = tokens.get(FlatNetworkSystemTags.L3_NETWORK_DHCP_IP_UUID_TOKEN)
         assert dhcpServerIp == pcmd.dhcpServerIp
 
         UsedIpVO dhcpIpVO = dbFindByUuid(dhcpServerIpUuid, UsedIpVO.class)
@@ -104,6 +194,20 @@ class OneVmDhcp extends SubCase {
         assert dhcpIpVO.netmask == vmNic.netmask
         assert dhcpIpVO.gateway == vmNic.gateway
         assert dhcpIpVO.l3NetworkUuid == l3.uuid
+    }
+
+    void testSetDhcpWhenCreateVm() {
+        testSetDhcpWhenVmOperations {
+            ImageSpec image = env.specByName("image")
+            InstanceOfferingSpec instanceOffering = env.specByName("instanceOffering")
+
+            vm = createVmInstance {
+                name = "vm"
+                imageUuid = image.inventory.uuid
+                l3NetworkUuids = [l3.uuid]
+                instanceOfferingUuid = instanceOffering.inventory.uuid
+            }
+        }
     }
 
     @Override
