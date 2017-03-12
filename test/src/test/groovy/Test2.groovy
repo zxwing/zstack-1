c7e04cd86d167119c1b8560d74f1bb3e11e307e8
Frank.Zhang <xin.zhang@mevoco.com>
2017-03-12 12:58:18 -0400

    add i18n to error code

--- test/src/test/groovy/Test2.groovy
+++ test/src/test/groovy/Test2.groovy
@@ -9,6 +9,7 @@ import org.junit.Test
 class Test2 {
     @Test
     void test() {
+        /*
         def fin = new FileInputStream("/root/VmInstanceBase.java")
 
         new VoidVisitorAdapter() {
@@ -16,10 +17,17 @@ class Test2 {
             void visit(MethodCallExpr n, Object arg) {
                 super.visit(n, arg)
                 if (n.getNameAsString() == "format") {
-                    System.out.println(n.getArguments())
+                    List<String> args = n.arguments.collect {
+                        "${it.toString()} : ${it.class}"
+                    }
+                    System.out.println(args)
                 }
                 //System.out.println(n.getNameAsString() + " ${arg?.class}")
             }
         }.visit(JavaParser.parse(fin), null)
+        */
+
+        def s = "The image[uuid:%s] is on the backup storage[uuid:%s, type:%s] that requires to work with primary storage[uuids:%s],however, no host found suitable to work with those primary storage"
+
     }
 }
