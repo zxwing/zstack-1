kuiwuluo/zstack

working on api progress

header/src/main/java/org/zstack/header/core/progress/TaskProgressVO_.java
@@ -1,0 +1,12 @@
+package org.zstack.header.core.progress;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+/**
+ * Created by xing5 on 2017/3/20.
+ */
+@StaticMetamodel(TaskProgressVO.class)
+public class TaskProgressVO_ {
+    public static volatile SingularAttribute<Long, String> id;
+}

zxwing/zstack-1

Conflicts:
	core/src/main/java/org/zstack/core/progress/ProgressReportService.java
	plugin/ceph/src/main/java/org/zstack/storage/ceph/backup/CephBackupStorageBase.java

header/src/main/java/org/zstack/header/core/progress/TaskProgressVO_.java
@@ -1,0 +1,20 @@
+package org.zstack.header.core.progress;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+/**
+ * Created by xing5 on 2017/3/20.
+ */
+@StaticMetamodel(TaskProgressVO.class)
+public class TaskProgressVO_ {
+    public static volatile SingularAttribute<Long, Long> id;
+    public static volatile SingularAttribute<Long, String> taskUuid;
+    public static volatile SingularAttribute<Long, String> apiId;
+    public static volatile SingularAttribute<Long, String> parentUuid;
+    public static volatile SingularAttribute<Long, TaskType> type;
+    public static volatile SingularAttribute<Long, String> content;
+    public static volatile SingularAttribute<Long, String> arguments;
+    public static volatile SingularAttribute<Long, String> managementUuid;
+    public static volatile SingularAttribute<Long, Long> time;
+}