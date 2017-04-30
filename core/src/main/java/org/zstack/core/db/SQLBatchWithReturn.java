789215f20c84db3e1d059e9556a2087127d7c565
Frank.Zhang <xin.zhang@mevoco.com>
2017-04-29 23:16:21 -0400

    add ResourceVO

--- core/src/main/java/org/zstack/core/db/SQLBatchWithReturn.java
+++ core/src/main/java/org/zstack/core/db/SQLBatchWithReturn.java
@@ -1,11 +1,18 @@
 package org.zstack.core.db;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by xing5 on 2017/3/4.
  */
+@Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
 public abstract class SQLBatchWithReturn<T> {
+    @Autowired
+    protected DatabaseFacade databaseFacade;
+
     protected abstract T scripts();
 
     protected SQL sql(String text) {
@@ -22,6 +29,19 @@ public abstract class SQLBatchWithReturn<T> {
 
     protected Q q(Class clz) {
         return Q.New(clz);
+    }
+
+    protected <K> K persist(K k) {
+        databaseFacade.getEntityManager().persist(k);
+        return k;
+    }
+
+    protected void flush() {
+        databaseFacade.getEntityManager().flush();
+    }
+
+    protected <K> K findByUuid(String uuid, Class<K> clz) {
+        return databaseFacade.getEntityManager().find(clz, uuid);
     }
 
     @Transactional
